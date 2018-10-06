cd700ac65cb4e97380c0c49a65037be02b896abc
Karolina Bielańska <adamowska.k@gmail.com>
2018-10-06 14:42:32 +0200

    New Private and Company User Creation Log in and Mentor booking inkl. Payment + Flight booking and printing screenshot from invoice

--- src/test/java/phptravel/pages/FlightSelectionPage.java
+++ src/test/java/phptravel/pages/FlightSelectionPage.java
@@ -1,6 +1,7 @@
 package phptravel.pages;
 
 import common.BasePage;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
